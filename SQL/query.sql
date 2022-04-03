65cbc2ce88c3c3e352a31273f5d5cad5318e1f39
Haifaa Abushaaban <abushaah@fred.socs.uoguelph.ca>
2022-04-03 17:32:05 -0400

    Fixed 2 and 3 to display specific columns

--- SQL/query.sql
+++ SQL/query.sql
@@ -5,8 +5,8 @@ SELECT * FROM FILE order by file_name;
 SELECT * FROM MODIFICATION order by mod_date;
 
 -- 3
-SELECT * FROM FILE where '2022-04-01 01:47:17' <= creation_time AND creation_time <= '2022-04-07 01:47:17' order by file_size;
+SELECT file_name, file_size, mod_date FROM FILE, MODIFICATION where '2022-04-01 01:47:17' <= creation_time AND creation_time <= '2022-04-07 01:47:17' order by file_size;
 /* where the first date is the start date and the second date is the end date*/
 
 -- 4
-SELECT * FROM DOWNLOAD ORDER BY d_time DESC LIMIT 1;
+SELECT file_name, d_time FROM FILE, DOWNLOAD ORDER BY d_time DESC LIMIT 1;
